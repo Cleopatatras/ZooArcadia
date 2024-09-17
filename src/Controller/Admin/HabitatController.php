c86ebd36349b2fdc17e1fdf44071f5063e501c55
Anne Choulet <anne.choulet@free.fr>
2024-09-17 15:21:28 +0200

    feat new branch

--- src/Controller/Admin/HabitatController.php
+++ src/Controller/Admin/HabitatController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use app\Entity\Habitats;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\AddhabitatFormType;
 
 
 #[Route(path: "/admin/habitat", name: "app_admin_habitat_")]
@@ -21,8 +23,12 @@ class HabitatController extends AbstractController
     #[Route('/ajouter', name: 'add')]
     public function addhabitat(): Response
     {
+        $habitat = new Habitat();
+
+        $habitatForm = $this->createForm(AddhabitatFormType::class, $habitat);
+
         return $this->render('admin/habitat/add.html.twig', [
-            'controller_name' => 'addHabitatController',
+            'habitatForm' => $habitatForm->createView(),
         ]);
     }
 }
